92434cb5fd12770cb42688cf5e73551af1de0461
gary <a27487770@gmail.com>
2024-06-01 17:53:14 +0000

    npm install primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: [
+    '@nuxt/eslint',
+    'nuxt-primevue',
+  ],
   eslint: {
     config: {
       stylistic: true, // <---
     },
   },
+  css: [
+    'primevue/resources/themes/aura-light-green/theme.css',
+    'primeicons/primeicons.css',
+  ],
 })
